87427f74c6b4dd424ca6841bacad20bdcd4caf36
osten-antonio <ostenantonio1@gmail.com>
2026-03-02 14:34:16 +0700

    feat: integrate Swagger for API documentation with swagger-jsdoc and swagger-ui-express

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,9 +1,29 @@
 import type { Application, Request, Response } from 'express';
 
+
 const express = require('express');
+const swaggerJsdoc = require('swagger-jsdoc');
+const swaggerUi = require('swagger-ui-express');
 
 const app: Application = express();
 const port = 3000;
+
+
+const options = {
+  definition: {
+    openapi: '3.0.0',
+    info: {
+      title: 'WADS-FP calculator',
+      version: '1.0.0',
+    },
+  },
+  apis: ['./src/index.ts'], 
+};
+
+const openapiSpecification = swaggerJsdoc(options);
+
+
+app.use('/docs', swaggerUi.serve, swaggerUi.setup(openapiSpecification));
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Welcome to Express & TypeScript Server');
