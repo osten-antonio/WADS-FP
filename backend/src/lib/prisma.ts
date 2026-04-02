2a86eec9502249d1362d4635c4485d1c222de02f
osten-antonio <ostenantonio1@gmail.com>
2026-04-02 21:16:44 +0700

    chore: update PrismaClient initialization to use PrismaPg adapter

--- backend/src/lib/prisma.ts
+++ backend/src/lib/prisma.ts
@@ -1,10 +1,12 @@
-import { PrismaClient } from "@prisma/client";
+import { PrismaPg } from '@prisma/adapter-pg';
+import { PrismaClient } from '../../generated/prisma/client';
+
 
 const globalForPrisma = globalThis as unknown as {
   prisma: InstanceType<typeof PrismaClient> | undefined;
 };
 
-export const prisma = globalForPrisma.prisma ?? new PrismaClient();
+export const prisma = globalForPrisma.prisma ?? new PrismaClient({adapter: new PrismaPg({ connectionString: process.env.DATABASE_URL })});
 
 if (process.env.NODE_ENV !== "production") {
   globalForPrisma.prisma = prisma;
