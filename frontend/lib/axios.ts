da76de5c1b5cfa61efb049cb380cfae6c55acb4d
osten-antonio <ostenantonio1@gmail.com>
2026-05-26 00:14:17 +0700

    chore: enhance backend and frontend configurations with environment variables for dynamic port and hostname settings

--- frontend/lib/axios.ts
+++ frontend/lib/axios.ts
@@ -1,6 +1,11 @@
+import "server-only";
 import axios from 'axios';
 
+const backendProtocol = process.env.BACKEND_PROTOCOL ?? "http";
+const backendHostname = process.env.BACKEND_HOSTNAME ?? "localhost";
+const backendPort = process.env.BACKEND_PORT ?? "8000";
+const backendUrl = `${backendProtocol}://${backendHostname}:${backendPort}`;
 export const backendApi = axios.create({
-  baseURL: process.env.BACKEND_URL ?? "http://localhost:8000",
+  baseURL: backendUrl,
   timeout: 10000,
 });
